6f45bc973ab9a3107ccbfb6b0eb24e0da8699444
Tosh Velaga <toshvelaga@gmail.com>
2021-11-22 23:40:11 -0700

    add vs code config

--- client/src/routes/Room.js
+++ client/src/routes/Room.js
@@ -1,74 +1,86 @@
-import React, { useEffect, useRef, useState } from "react";
-import io from "socket.io-client";
-import Peer from "simple-peer";
-import styled from "styled-components";
+import React, { useEffect, useRef, useState } from 'react'
+import io from 'socket.io-client'
+import Peer from 'simple-peer'
+import styled from 'styled-components'
 
 const Container = styled.div`
-    padding: 20px;
-    display: flex;
-    height: 100vh;
-    width: 90%;
-    margin: auto;
-    flex-wrap: wrap;
-`;
+  padding: 20px;
+  display: flex;
+  height: 100vh;
+  width: 90%;
+  margin: auto;
+  flex-wrap: wrap;
+`
 
 const StyledVideo = styled.video`
-    height: 40%;
-    width: 50%;
-`;
+  height: 40%;
+  width: 50%;
+`
 
 const Video = (props) => {
-    const ref = useRef();
+  const ref = useRef()
 
-    useEffect(() => {
-        props.peer.on("stream", stream => {
-            ref.current.srcObject = stream;
-        })
-    }, []);
+  useEffect(() => {
+    props.peer.on('stream', (stream) => {
+      ref.current.srcObject = stream
+    })
+  }, [])
 
-    return (
-        <StyledVideo playsInline autoPlay ref={ref} />
-    );
+  return <StyledVideo playsInline autoPlay ref={ref} />
 }
 
-
 const videoConstraints = {
-    height: window.innerHeight / 2,
-    width: window.innerWidth / 2
-};
+  height: window.innerHeight / 2,
+  width: window.innerWidth / 2,
+}
 
 const Room = (props) => {
-    const [peers, setPeers] = useState([]);
-    const socketRef = useRef();
-    const userVideo = useRef();
-    const peersRef = useRef([]);
-    const roomID = props.match.params.roomID;
+  const [peers, setPeers] = useState([])
+  const socketRef = useRef()
+  const userVideo = useRef()
+  const peersRef = useRef([])
+  const roomID = props.match.params.roomID
 
-    useEffect(() => {
-        socketRef.current = io.connect("/");
-        navigator.mediaDevices.getUserMedia({ video: videoConstraints, audio: true }).then(stream => {
-           
+  useEffect(() => {
+    socketRef.current = io.connect('/')
+    navigator.mediaDevices
+      .getUserMedia({ video: videoConstraints, audio: true })
+      .then((stream) => {
+        userVideo.current.srcObject = stream
+        socketRef.current.emit('join room')
+        socketRef.current.on('all users', (users) => {
+          const peers = []
+          users.forEach((userID) => {
+            const peer = createPeer(userID, socketRef.current.id, stream)
+            peersRef.current.push({
+              peerID: userID,
+              peer,
+            })
+            peers.push(peer)
+            setPeers(peers)
+          })
         })
-    }, []);
+      })
+  }, [])
 
-    function createPeer(userToSignal, callerID, stream) {
-       
-    }
+  function createPeer(userToSignal, callerID, stream) {
+    const peer = new Peer({
+      initiator: true,
+      trickle: false,
+      stream,
+    })
+  }
 
-    function addPeer(incomingSignal, callerID, stream) {
-       
-    }
+  function addPeer(incomingSignal, callerID, stream) {}
 
-    return (
-        <Container>
-            <StyledVideo muted ref={userVideo} autoPlay playsInline />
-            {peers.map((peer, index) => {
-                return (
-                    <Video key={index} peer={peer} />
-                );
-            })}
-        </Container>
-    );
-};
+  return (
+    <Container>
+      <StyledVideo muted ref={userVideo} autoPlay playsInline />
+      {peers.map((peer, index) => {
+        return <Video key={index} peer={peer} />
+      })}
+    </Container>
+  )
+}
 
-export default Room;
+export default Room
